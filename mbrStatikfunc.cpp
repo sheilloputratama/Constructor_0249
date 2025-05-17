fd49d774481506e5a09a4a09dd467cde1b0d4ea8
sheilloputratama <sheilloggendut@gmail.com>
2025-05-17 19:04:46 +0700

    membuat static methods

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -5,5 +5,14 @@ using namespace std;
 class mahasiswa{
 private:
     static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) {nim=pNim; /*Definisi Function*/}
+    static int getNim(){return nim; /*Definisi Function*/}
 
     
