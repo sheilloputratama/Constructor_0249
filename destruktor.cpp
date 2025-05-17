5753422d80e1bf8ee3991de75a8a1e8040db17de
sheilloputratama <sheilloggendut@gmail.com>
2025-05-17 16:50:03 +0700

    membuat definisi member function

--- destruktor.cpp
+++ destruktor.cpp
@@ -5,3 +5,14 @@ class angka {
 private:
     int *arr;
     int panjang;
+public:
+    angka(int); //Constructor
+    ~angka(); //Destructor
+    void cetakData();
+    void isiData();
+};
+//Definisi member Functon
+angka:: angka (int i){ //Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
