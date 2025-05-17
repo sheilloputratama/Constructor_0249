554554d70429fcc9f375b55b0a6650be73631f09
sheilloputratama <sheilloggendut@gmail.com>
2025-05-17 16:52:22 +0700

    memanggil constructor

--- destruktor.cpp
+++ destruktor.cpp
@@ -24,3 +24,20 @@ angka::~angka() { //Destructor
     delete[]arr;
     cout<<"Alamat Array Sudah Dilepaskan"<<endl;
 }
+
+void angka:: cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<"=";cin>>arr[i];
+    }
+    cout<<endl;
+}
+
+int main() {
+    angka belajarcpp(3); //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); // construktor dipanggil
