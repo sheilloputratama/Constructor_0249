f6de71ae9fe5556734cca447217794e3b8abeb2c
sheilloputratama <sheilloggendut@gmail.com>
2025-05-17 19:04:15 +0700

    membuat static variable

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -5,35 +5,5 @@ using namespace std;
 class mahasiswa{
 private:
     static int nim;
-public:
-    int id;
-    string nama;
 
-    void setID();
-    void printAll();
-
-    static void setNim(int pNim) {nim=pNim; /*Definisi Function*/}
-    static int getNim(){return nim; /*Definisi Function*/}
-
-    mahasiswa (string pnama): nama (pnama) {setID();}
-};
-
-int mahasiswa::nim=0; 
-
-void mahasiswa::setID(){
-    id = ++nim;
-}
-
-void mahasiswa::printAll(){
-    cout<<"Nama= "<<nama<<endl;
-    cout<<"ID = "<<id<<endl;
-    cout<<endl;
-}
-
-int main(){
-    mahasiswa mhs1("Sri Dadi");
-    mahasiswa mhs2("Budi Jatmiko");
-    mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim" 
-
-}
     
