928ca9cd5f1f419b534fb2695b17e506592103d0
sheilloputratama <sheilloggendut@gmail.com>
2025-05-17 19:13:22 +0700

    membuat implementasi method cetak

--- Konstruktor.cpp
+++ Konstruktor.cpp
@@ -18,3 +18,19 @@ mahasiswa:: mahasiswa(){
     nim = 0;
     nama = "";
 }
+
+mahasiswa :: mahasiswa (int iNim){
+nim=iNim;
+}
+
+mahasiswa :: mahasiswa (string iNama) {
+nama=iNama;
+}
+
+mahasiswa :: mahasiswa (int iNim, string iNama){
+nim=iNim;
+nama=iNama;
+}
+void mahasiswa:: cetak(){
+cout<<endl<< "Nim =" <<nim<<endl;
+cout<<" Nama"<<nama<<endl;
